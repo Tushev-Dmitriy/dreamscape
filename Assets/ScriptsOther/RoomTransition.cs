d5b0f32a5d2bfb9b7e24fca0561e0006543440b5
albnzhum <albnzhum@gmail.com>
2024-12-12 14:18:40 +0500

    Added models

--- Assets/ScriptsOther/RoomTransition.cs
+++ Assets/ScriptsOther/RoomTransition.cs
@@ -8,16 +8,30 @@ public class RoomTransition : MonoBehaviour
     public bool isRoom = false;
     public int roomId = 3;
 
+    [Header("Room")]
     [SerializeField] private LoadEventChannelSO loadRoomEventChannel;
     [SerializeField] private IntEventChannelSO currentRoomIdEvent;
     [SerializeField] private GameSceneSO gameScene;
+    
+    [Header("Hub")]
+    [SerializeField] private LoadEventChannelSO loadHubEventChannel;
+    [SerializeField] private GameSceneSO hubScene;
+    
+   /* [Header("Hub")]
+    [SerializeField] private */
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            currentRoomIdEvent.RaiseEvent(roomId);
-            loadRoomEventChannel.RaiseEvent(gameScene, true, true);
+            if (!isRoom) {
+                currentRoomIdEvent.RaiseEvent(roomId);
+                loadRoomEventChannel.RaiseEvent(gameScene, true, true);
+            }
+            else
+            {
+                loadHubEventChannel.RaiseEvent(hubScene, true, true);
+            }
         }
     }
 }
